bbdedcbbe31bc71fb6131f8e3fa0ff4cb98143d1
Tomas Quintero <tomasq@gmail.com>
2020-05-23 01:10:22 -0400

    tzdata

--- minecraft/dockercontrol.sh
+++ minecraft/dockercontrol.sh
@@ -26,7 +26,7 @@ function start_container() {
     --name "$CONTAINER_NAME" \
     --memory="5g" \
     --cpus="2" \
-    -e TZ=America/New_York \
+    -e "TZ=America/New_York" \
     -p 25565:25565 \
     -v minecraft_data:/data \
     "$IMAGE_NAME"
